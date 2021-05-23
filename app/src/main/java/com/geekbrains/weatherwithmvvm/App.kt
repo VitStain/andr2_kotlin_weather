a7e0a4695b7783234fd285c3e1eb086c7ea77b81
VitStain <vitalissim@yandex.ru>
2021-05-23 18:55:47 +0300

    HW_lesson8

---
+++ app/src/main/java/com/geekbrains/weatherwithmvvm/App.kt
@@ -0,0 +1,14 @@
+package com.geekbrains.weatherwithmvvm
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        appInstance = this
+    }
+
+    companion object {
+        lateinit var appInstance: App
+    }
+}
---
+++ app/src/main/java/com/geekbrains/weatherwithmvvm/model/database/HistoryEntity.kt
@@ -0,0 +1,12 @@
+package com.geekbrains.weatherwithmvvm.model.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class HistoryEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val city: String,
+    val temperature: Int,
+    val condition: String
+)
